a7c3a2d1bad0b7720c29b9f43588f949ed5e1e81
Eihapkaram <eihapkaram9090@gmail.com>
2025-04-13 17:06:44 -0700

    Update CatigoryProduct.vue
    t

--- src/views/CatigoryProduct.vue
+++ src/views/CatigoryProduct.vue
@@ -1,97 +1,108 @@
 <template>
-  <v-row id="con" class="d-flex flex-row">
-    <v-progress-linear
-      color="blue"
-      indeterminate
-      v-if="load"
-    ></v-progress-linear>
-    <v-col cols="12"
-      ><h2>{{ this.$route.params.catigory }}</h2></v-col
-    >
-
-    <v-card
-      width="300"
-      height="550px"
-      v-for="pro in this.catigoryProducts"
-      :key="pro.id"
-    >
-      <v-row>
-        <v-btn id="lovebtn" @focus="lovefun(pro)" icon="mdi-heart"></v-btn
-        ><v-spacer></v-spacer>
-        <span id="availbel" class="d-flex">{{ pro.availabilityStatus }}</span>
-      </v-row>
-      <div
-        id="parantimg1"
-        style="overflow: hidden; height: fit-content; position: relative"
+  <v-lazy
+    :min-height="200"
+    :options="{ threshold: 0.5 }"
+    transition="fade-transition"
+  >
+    <v-row id="con" class="d-flex flex-row">
+      <v-progress-linear
+        color="blue"
+        indeterminate
+        v-if="load"
+      ></v-progress-linear>
+      <v-col cols="12"
+        ><h2>{{ this.$route.params.catigory }}</h2></v-col
       >
-        <v-btn
-          @click="this.Emitter.emit('dilog', pro)"
-          rounded
-          variant="outliened"
-          id="quick"
-          ripple
-          >quick view</v-btn
-        >
-        <img id="big-img" width="200px" :src="pro.thumbnail" />
-      </div>
-      <v-card-item>
-        <v-card-title>{{ pro.title }}</v-card-title>
-        <v-rating
-          v-model="pro.rating"
-          ripple
-          readonly
-          :length="5"
-          :size="20"
-          active-color="orange"
-          class="d-flex"
-        /><br />
-        <v-card-subtitle class="d-flex text-h6" style="color: brown"
-          >price : {{ pro.price }} $
-        </v-card-subtitle>
-        <v-card-subtitle class="d-flex"
-          >brand : {{ pro.brand }}
-        </v-card-subtitle>
-        <v-card-text>
-          <swiper
-            :scrollbar="{
-              hide: true,
-            }"
-            :pagination="{
-              clickable: true,
-            }"
-            :navigation="true"
-            :mousewheel="true"
-            :keyboard="true"
-            :slides-per-view="4"
-            :spaceBetween="2"
-            :modules="modules"
-            class=""
-          >
-            <swiper-slide v-for="(prox, index) in pro.images" :key="index">
-              <img id="my-galleryimg" height="50px" width="50px" :src="prox" />
-            </swiper-slide>
-          </swiper>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn
-            @click="
-              $router.push({ name: 'derilse', params: { idparam: pro.id } })
-            "
-            rounded
-            >show-detilse</v-btn
+
+      <v-card
+        width="300"
+        height="550px"
+        v-for="pro in this.catigoryProducts"
+        :key="pro.id"
+      >
+        <v-row>
+          <v-btn id="lovebtn" @focus="lovefun(pro)" icon="mdi-heart"></v-btn
           ><v-spacer></v-spacer>
+          <span id="availbel" class="d-flex">{{ pro.availabilityStatus }}</span>
+        </v-row>
+        <div
+          id="parantimg1"
+          style="overflow: hidden; height: fit-content; position: relative"
+        >
           <v-btn
-            @click="(pro.quantity = 1), this.Additem(pro)"
+            @click="this.Emitter.emit('dilog', pro)"
             rounded
+            variant="outliened"
+            id="quick"
             ripple
-            elevation="2"
-            style=""
-            icon="mdi-cart"
-          ></v-btn>
-        </v-card-actions>
-      </v-card-item>
-    </v-card>
-  </v-row>
+            >quick view</v-btn
+          >
+          <img id="big-img" width="200px" :src="pro.thumbnail" />
+        </div>
+        <v-card-item>
+          <v-card-title>{{ pro.title }}</v-card-title>
+          <v-rating
+            v-model="pro.rating"
+            ripple
+            readonly
+            :length="5"
+            :size="20"
+            active-color="orange"
+            class="d-flex"
+          /><br />
+          <v-card-subtitle class="d-flex text-h6" style="color: brown"
+            >price : {{ pro.price }} $
+          </v-card-subtitle>
+          <v-card-subtitle class="d-flex"
+            >brand : {{ pro.brand }}
+          </v-card-subtitle>
+          <v-card-text>
+            <swiper
+              :scrollbar="{
+                hide: true,
+              }"
+              :pagination="{
+                clickable: true,
+              }"
+              :navigation="true"
+              :mousewheel="true"
+              :keyboard="true"
+              :slides-per-view="4"
+              :spaceBetween="2"
+              :modules="modules"
+              class=""
+            >
+              <swiper-slide v-for="(prox, index) in pro.images" :key="index">
+                <img
+                  id="my-galleryimg"
+                  height="50px"
+                  width="50px"
+                  :src="prox"
+                />
+              </swiper-slide>
+            </swiper>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn
+              @click="
+                $router.push({ name: 'derilse', params: { idparam: pro.id } })
+              "
+              rounded
+              >show-detilse</v-btn
+            ><v-spacer></v-spacer>
+            <v-btn
+              @click="(pro.quantity = 1), this.Additem(pro)"
+              rounded
+              ripple
+              elevation="2"
+              style=""
+              icon="mdi-cart"
+            ></v-btn>
+          </v-card-actions>
+        </v-card-item>
+      </v-card>
+    </v-row>
+  </v-lazy>
 </template>
 <script>
 import { mystore } from "@/store";
